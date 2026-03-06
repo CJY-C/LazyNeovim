e557aa58dd17d0d3273b6af571c843e4a14863b5
CJY-C <masarity@outlook.com>
2026-03-06 12:24:48 +0800

    feat(config): lsp customize

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,3 +11,10 @@ opt.encoding = "utf-8"
 opt.fileencodings = "utf-8,ucs-bom,gbk,cp936,gb2312,gb18030,sjis"
 
 opt.termguicolors = true
+
+vim.g.lazyvim_python_lsp = "basedpyright"
+
+-- 关闭内建 LSP 状态通知弹框
+vim.lsp.handlers["$/progress"] = function() end
+vim.lsp.handlers["window/progress"] = function() end
+vim.lsp.handlers["window/showMessage"] = function() end
--- lua/plugins/overwrite.lua
+++ lua/plugins/overwrite.lua
@@ -74,4 +74,41 @@ return
       enabled = false,
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        accept = {
+          -- experimental auto-brackets support
+          auto_brackets = {
+            enabled = false,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+        ghost_text = {
+          -- enabled = vim.g.ai_cmp,
+          enabled = false,
+        },
+      },
+    },
+  },
 }
